2281e8ef42ba3ab563d904d5111eb21d32c02d39
tise.c <tise.c@gmail.com>
2014-07-07 16:22:37 +0800

    add config/puma.rb

---
+++ config/puma.rb
@@ -0,0 +1,15 @@
+root = "/home/tiny/campo-h5/current/"
+environment 'production'
+#directory '/home/tiny/campo-h5'
+
+workers 0
+threads 0, 16
+
+#rackup "#{root}/config.ru"
+
+bind  "unix:///tmp/puma.sock"
+pidfile "#{root}/tmp/pids/puma.pid"
+state_path "#{root}/tmp/pids/puma.state"
+daemonize true
+
+preload_app!
