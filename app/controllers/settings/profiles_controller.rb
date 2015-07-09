422e25b2b4dc63fc50c752a1789a0d9fb718292f
tise.c <tiseheaini@gmail.com>
2015-07-09 18:59:45 +0800

    settings#account username 取消修改

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -3,7 +3,7 @@ class Settings::ProfilesController < Settings::ApplicationController
   end
 
   def update
-    if @user.update_attributes params.require(:user).permit(:name, :bio, :avatar, :remove_avatar)
+    if @user.update_attributes params.require(:user).permit(:bio, :avatar, :remove_avatar)
       flash[:success] = I18n.t('settings.profiles.flashes.successfully_updated')
       redirect_to settings_profile_url
     else
